6dd6ae8191ba2089bd003883f29af7dffe76c041
crowdaccent <mahendra@crowdaccent.com>
2011-10-15 07:34:28 +0530

    Adding intial version of polling daemons which collect information from endpoints

---
+++ src/daemons/status/MTurkWorkerStatus.java
@@ -0,0 +1,10 @@
+package crowdaccent.daemons.status;
+
+/**
+ * Daemon provides the current status of the assigned tasks to workers.
+ * @author mkutare
+ *
+ */
+public class MTurkWorkerStatus {
+
+}
