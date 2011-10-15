23844c9b22f80f986f09bb22cd24adc51b3f40f4
crowdaccent <mahendra@crowdaccent.com>
2011-10-15 07:11:54 +0530

    Initial version for orchestration layer to interact with multiple underlying platforms

---
+++ src/orchestration/Driver.java
@@ -0,0 +1,11 @@
+package crowdaccent.orchestration;
+
+/**
+ * Generalizes the interaction with multiple underlying turker platforms
+ * @author mkutare
+ *
+ */
+public class Driver {
+
+	
+}
---
+++ src/orchestration/Hypervisor.java
@@ -0,0 +1,21 @@
+package crowdaccent.orchestration;
+
+/**
+ * Performs multiplexing of micro tasks with underlying platforms
+ * @author mkutare
+ *
+ */
+public class Hypervisor {
+
+	public Hypervisor() {
+
+	}
+	
+	/**
+	 * Provides multiple algorithms for scheduling human computations
+	 * such as credit based fair proportional and gang scheduling
+	 */
+	public void scheduleHumanComputation() {
+		
+	}
+}
