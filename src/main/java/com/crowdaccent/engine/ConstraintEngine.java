6344ab837438aa1de7e4ca734cc3045fe9539463
crowdaccent <mahendra@crowdaccent.com>
2011-10-20 18:54:18 +0530

    Removing the typo in ConstraintEngine class name

---
+++ src/main/java/com/crowdaccent/engine/ConstraintEngine.java
@@ -0,0 +1,42 @@
+package com.crowdaccent.engine;
+
+/**
+ * Constraint engine to calculate the tradeoffs for human tasks
+ * @author mkutare
+ *
+ */
+public class ConstraintEngine {
+	
+	public ConstraintEngine() {
+		
+	}
+	
+	public void optimiseCost() {
+		
+	}
+
+	public void optimizeSpeed() {
+		
+	}
+	
+	public void optimizeAccuracy() {
+		
+	}
+
+	public void optimizeCostAndSpeed() {
+	
+	}
+
+	public void optimizeCostAndAccuracy() {
+	
+	}
+
+	public void optimizeSpeedAndAccuracy() {
+	
+	}
+
+    public void optimizeCostSpeedAndAccuracy() {
+	
+	}
+
+}
