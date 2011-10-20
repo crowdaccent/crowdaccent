f715e2bb0c9715a7d2bf2bf2893aeffd1edb9949
crowdaccent <mahendra@crowdaccent.com>
2011-10-20 18:53:18 +0530

    Initial api code and datastructures for amazon mechanical turk

---
+++ src/main/java/com/crowdaccent/amazon/MTurkApi.java
@@ -0,0 +1,19 @@
+package com.crowdaccent.amazon;
+
+public class MTurkApi {
+	
+	public MTurkApi () {
+		
+	}
+
+	public String createHIT(String operation, String hitTypeId, String question, int lifeTimeInSeconds, int maxAssignments, String requesterAnnotation ) {
+		
+		return null;
+	}
+
+	public String createHIT(String operation, String title, String description, String question, Reward reward, int assignmentDurationInSecs, int lifeTimeInSeconds, String keywords, int maxAssignments, int autoApprovalDelaySecs,  QualificationRequirement qualificationRequirement, String requestorAnnotation) {
+		
+		return null;
+	}
+
+}
