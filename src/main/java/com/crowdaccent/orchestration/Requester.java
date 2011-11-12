f9c2e61eb5c0db64168b142b3f6cebd4ae1875f4
Mahendra Kutare <mahendra.kutare@gmail.com>
2011-11-12 14:14:18 -0800

    Added qualitification type and creating introduction hit with embedded image

--- src/main/java/com/crowdaccent/orchestration/Requester.java
+++ src/main/java/com/crowdaccent/orchestration/Requester.java
@@ -2,6 +2,8 @@ package com.crowdaccent.orchestration;
 
 import com.amazonaws.mturk.requester.HIT;
 import com.amazonaws.mturk.requester.QualificationRequirement;
+import com.amazonaws.mturk.requester.QualificationType;
+import com.amazonaws.mturk.requester.QualificationTypeStatus;
 import com.amazonaws.mturk.service.axis.RequesterServiceRaw;
 import com.amazonaws.mturk.service.exception.ServiceException;
 import com.amazonaws.mturk.util.ClientConfig;
@@ -80,6 +82,44 @@ public class Requester extends RequesterServiceRaw {
     
 	}
 
+	public HIT createIntroductionHITWithImage(String hitTypeId, String title, String description, String keywords, String question, Double reward, 
+            Long assignmentDurationInSeconds, Long autoApprovalDelayInSeconds, Long lifetimeInSeconds, Integer maxAssignments, 
+            String requesterAnnotation, QualificationRequirement[] qualificationRequirements, String[] responseGroup,
+            String displayName, String[] items, Overview oContent, Question[] qContent)
+                    throws ServiceException {       
+        return super.createHIT(hitTypeId, // HITTypeId 
+        title, 
+        description, 
+        keywords, // keywords 
+        this.getIntroductionHITQuestionWithImage(oContent, qContent), 
+        reward, 
+        assignmentDurationInSeconds, 
+        autoApprovalDelayInSeconds, 
+        lifetimeInSeconds, 
+        maxAssignments, 
+        requesterAnnotation, // requesterAnnotation 
+        qualificationRequirements,
+        responseGroup  // responseGroup
+      );
+	}
+	
+	public QualificationType createQualificationType(String name, String keywords, String description,
+      QualificationTypeStatus status, Long retryDelayInSeconds, String test, String answerKey,
+      Long testDurationInSeconds, Boolean autoGranted, Integer autoGrantedValue) 
+    throws ServiceException { 
+    
+    return super.createQualificationType(name, 
+            keywords, 
+            description, 
+            status, 
+            retryDelayInSeconds, 
+            test, 
+            answerKey, 
+            testDurationInSeconds, 
+            autoGranted, 
+            autoGrantedValue);
+	}
+	
 	public String getBasicFreeTextQuestion(String question) {
 		String q = "";
 		q += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
@@ -170,6 +210,99 @@ public class Requester extends RequesterServiceRaw {
         q += "    <IsRequired>" + qContent[1].isRequired() + "</IsRequired>";
         q += "    <QuestionContent>";
         q += "      <Title>" + qContent[1].getTitle() + "</Title>";
+        q += "      <Text>" + qContent[1].getQuestion() + "</Text>";
+        q += "    </QuestionContent>"; 
+        q += "    <AnswerSpecification>";
+        q += "      <SelectionAnswer>";
+        q += "      <MinSelectionCount>1</MinSelectionCount>";
+        q += "      <StyleSuggestion>radiobutton</StyleSuggestion>";
+        q += "      <Selections>";
+        q += "          <Selection>";
+        q += "              <SelectionIdentifier>Level0</SelectionIdentifier>";
+        q += "              <Text>"+qContent[1].getItems()[0]+"</Text>";
+        q += "          </Selection>";
+        q += "          <Selection>";
+        q += "              <SelectionIdentifier>Level1</SelectionIdentifier>";
+        q += "              <Text>"+qContent[1].getItems()[1]+"</Text>";
+        q += "          </Selection>";
+        q += "          <Selection>";
+        q += "              <SelectionIdentifier>Level2</SelectionIdentifier>";
+        q += "              <Text>"+qContent[1].getItems()[1]+"</Text>";
+        q += "          </Selection>";       
+        q += "      </Selections>";
+        q += "      </SelectionAnswer>";
+        q += "    </AnswerSpecification>"; 
+        q += "  </Question>";
+        q += "  <Question>";
+        q += "    <QuestionIdentifier>"+ qContent[2].getQuestionId()+ "</QuestionIdentifier>";
+        q += "    <QuestionContent>";
+        q += "      <Text>" + qContent[2].getQuestion() + "</Text>";
+        q += "    </QuestionContent>";
+        q += "    <AnswerSpecification>";
+        q += "      <FreeTextAnswer/>";
+        q += "    </AnswerSpecification>";
+        q += "  </Question>";
+        q += "</QuestionForm>";
+        return q;
+    }
+
+    public String getIntroductionHITQuestionWithImage(Overview oContent, Question[] qContent) {
+        String q = "";
+        q += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+        q += "<QuestionForm xmlns=\"http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd\">";
+        q += "  <Overview>";
+        q += "      <Title>" + oContent.getTitle() + "</Title>"; //Product Image and Category Validation
+        q += "      <Text>" + oContent.getInformation()[0] + "</Text>"; //Image URL:
+        q += "      <EmbeddedBinary>";
+        q += "          <EmbeddedMimeType>";
+        q += "              <Type>" + "image" + "</Type>"; //Image :
+        q += "              <SubType>" + "gif" + "</SubType>"; //Image :
+        q += "          </EmbeddedMimeType>";
+        q += "          <DataURL>" + oContent.getInformation()[1] + "</DataURL>"; //Data URL
+        q += "          <AltText>" + oContent.getInformation()[2] + "</AltText>"; //Text
+        q += "          <Width>" + oContent.getInformation()[3] + "</Width>"; //300
+        q += "          <Height>" + oContent.getInformation()[4] + "</Height>"; //400
+        q += "          <ApplicationParameter>";
+        q += "              <Name>" + oContent.getInformation()[5] + "</Name>"; //image-id
+        q += "              <Value>" + oContent.getInformation()[6] + "</Value>"; //1
+        q += "          </ApplicationParameter>";
+        q += "      </EmbeddedBinary>";
+        q += "      <Text>" + "Title :" + oContent.getInformation()[7] + "</Text>"; //Title:
+        q += "      <Text>" + "Description :" + oContent.getInformation()[8] + "</Text>"; //Description:
+        q += "  </Overview>"; 
+        q += "  <Question>"; 
+        q += "    <QuestionIdentifier>"+ qContent[0].getQuestionId()+"</QuestionIdentifier>";
+        q += "    <DisplayName> " + qContent[0].getDisplayName() + "</DisplayName>";
+        q += "    <IsRequired>" + qContent[0].isRequired() + "</IsRequired>";
+        q += "    <QuestionContent>";
+        q += "      <Text>" + qContent[0].getQuestion() + "</Text>";
+        q += "    </QuestionContent>"; 
+        q += "    <AnswerSpecification>";
+        q += "      <SelectionAnswer>";
+        q += "      <MinSelectionCount>3</MinSelectionCount>";
+        q += "      <StyleSuggestion>checkbox</StyleSuggestion>";
+        q += "      <Selections>";
+        q += "          <Selection>";
+        q += "              <SelectionIdentifier>1</SelectionIdentifier>";
+        q += "              <Text>"+qContent[0].getItems()[0]+"</Text>";
+        q += "          </Selection>";
+        q += "          <Selection>";
+        q += "              <SelectionIdentifier>2</SelectionIdentifier>";
+        q += "              <Text>"+qContent[0].getItems()[1]+"</Text>";
+        q += "          </Selection>";
+        q += "          <Selection>";
+        q += "              <SelectionIdentifier>3</SelectionIdentifier>";
+        q += "              <Text>"+qContent[0].getItems()[1]+"</Text>";
+        q += "          </Selection>";       
+        q += "      </Selections>";
+        q += "      </SelectionAnswer>";
+        q += "    </AnswerSpecification>"; 
+        q += "  </Question>";
+        q += "  <Question>"; 
+        q += "    <QuestionIdentifier>"+ qContent[1].getQuestionId()+"</QuestionIdentifier>";
+        q += "    <DisplayName> " + qContent[1].getDisplayName() + "</DisplayName>";
+        q += "    <IsRequired>" + qContent[1].isRequired() + "</IsRequired>";
+        q += "    <QuestionContent>";
         q += "      <Text>" + qContent[1].getQuestion() + "</Text>";
         q += "    </QuestionContent>"; 
         q += "    <AnswerSpecification>";
