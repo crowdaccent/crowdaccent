b227ea1fa014041c9a9127b76be6774f701d7fca
Mahendra Kutare <mahendra.kutare@gmail.com>
2011-11-24 01:47:08 -0800

    Modified gateway interface

--- src/main/java/com/crowdaccent/orchestration/gateway/Gateway.java
+++ src/main/java/com/crowdaccent/orchestration/gateway/Gateway.java
@@ -1,5 +1,10 @@
 package com.crowdaccent.orchestration.gateway;
 
+import com.amazonaws.mturk.requester.Assignment;
+import com.amazonaws.mturk.requester.GetAssignmentsForHITResult;
+import com.amazonaws.mturk.requester.GetReviewableHITsResult;
+import com.amazonaws.mturk.requester.HIT;
+
 public interface Gateway {
 
 	/**
@@ -29,6 +34,65 @@ public interface Gateway {
     */
    public HITResponse createIntroductionHITWithImage(HITRequest hrequest);
    
+   /**
+    * 
+    * @param hitTypeId
+    * @param pageNum
+    * @param pageSize
+    * @return
+    */
+   public GetReviewableHITsResult getReviewableHITsWithCreationTimeOrderAndPageDetails(String hitTypeId, Integer pageNum, Integer pageSize);
+   
+   /**
+    * 
+    * @param hitTypeId
+    * @param pageNum
+    * @param pageSize
+    * @return
+    */
+   public HIT[] getReviewableHITsDetailsWithCreationTimeSortOrderAndPageDetails(String hitTypeId, Integer pageNum, Integer pageSize);
+
+   /**
+    * 
+    * @param hitId
+    * @param pageNumber
+    * @param pageSize
+    * @param getFullResponse
+    * @return
+    */
+   public GetAssignmentsForHITResult getAllAssignmentsHITResults(String hitId, Integer pageNumber, Integer pageSize, boolean getFullResponse);
+
+   /**
+    * 
+    * @param hitId
+    * @param pageNumber
+    * @param pageSize
+    * @param getFullResponse
+    * @return
+    */
+   public Assignment[] getAllAssignmentsForHIT(String hitId, Integer pageNumber, Integer pageSize, boolean getFullResponse);
+
+   /**
+    * 
+    * @param hitId
+    * @param pageNumber
+    * @param pageSize
+    * @param getFullResponse
+    * @return
+    */
+   public GetAssignmentsForHITResult getSubmittedAssignmentsForHITResults(String hitId, Integer pageNumber, Integer pageSize, boolean getFullResponse);
+
+   /**
+    * 
+    * @param hitId
+    * @param pageNumber
+    * @param pageSize
+    * @param getFullResponse
+    * @return
+    */
+   public Assignment[] getSubmittedAssignmentsResultsForHIT(String hitId, Integer pageNumber, Integer pageSize, boolean getFullResponse);
+
+       
    /**
     * 
     * @param subject
